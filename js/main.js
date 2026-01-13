5b698c9e747155482b6c0a3cdccdb5cf0434daf9
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-01-13 15:10:44 +0100

    output for price

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-    
+const formVerifyer = document.getElementById("verifyer");
+const kmCounter = document.getElementById("km-counter");
+const kmVisible = document.getElementById("km-price");
+console.log(formVerifyer);
+formVerifyer.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let userKm = kmCounter.value;
+  let kmprice = userKm * 0.21;
+  console.log(kmprice);
+  kmVisible.innerText = kmprice;
+});
