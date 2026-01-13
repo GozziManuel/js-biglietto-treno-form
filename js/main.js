11c2e67ee294485b1bc84dd5f2408e9552b653bc
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-01-13 16:19:05 +0100

    added counter

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ const kmCounter = document.getElementById("km-counter");
 const kmVisible = document.getElementById("km-price");
 const announcer = document.getElementById("announcer");
 const ageFinder = document.getElementById("age-finder");
+const kmAnnouncer = document.getElementById("km-announcer");
+const kmAfter = document.getElementById("km-after");
 // FORM FUNCTION
 formVerifyer.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -14,20 +16,28 @@ formVerifyer.addEventListener("submit", function (e) {
   if (isNaN(userKm) == true) {
     announcer.innerText = "";
     kmVisible.innerText = "Inserire numero valido";
+    kmAnnouncer.innerText = "";
+    kmAfter.innerText = "";
   } else {
     //   if sales
     if (ageFinder.value === "over-65") {
       kmPrice = kmPrice - (kmPrice * 40) / 100;
       let kmPriceDecimal = kmPrice.toFixed(2);
       kmVisible.innerText = kmPriceDecimal;
+      kmAnnouncer.innerText = "i chilometri del viaggio sono";
+      kmAfter.innerText = userKm;
     } else if (ageFinder.value === "underage") {
       kmPrice = kmPrice - (kmPrice * 20) / 100;
       let kmPriceDecimal = kmPrice.toFixed(2);
       kmVisible.innerText = kmPriceDecimal;
+      kmAnnouncer.innerText = "i chilometri del viaggio sono";
+      kmAfter.innerText = userKm;
       // normal price
     } else {
       announcer.innerText = "Il prezzo è ";
       kmVisible.innerText = kmPrice;
+      kmAnnouncer.innerText = "i chilometri del viaggio sono";
+      kmAfter.innerText = userKm;
     }
   }
 });
