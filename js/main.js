4f12f06359598d56908911f7838854ff7c77e421
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-01-13 15:30:10 +0100

    added if

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 const formVerifyer = document.getElementById("verifyer");
 const kmCounter = document.getElementById("km-counter");
 const kmVisible = document.getElementById("km-price");
+const announcer = document.getElementById("announcer");
+const ageFinder = document.getElementById("age-finder");
 console.log(formVerifyer);
 formVerifyer.addEventListener("submit", function (e) {
   e.preventDefault();
   let userKm = kmCounter.value;
   let kmprice = userKm * 0.21;
   console.log(kmprice);
+  if (ageFinder.value === "over-65") {
+    kmprice = kmprice - (kmprice * 40) / 100;
+  } else if (ageFinder.value === "underage") {
+    kmprice = kmprice - (kmprice * 20) / 100;
+  }
+  announcer.innerText = "Il prezzo è ";
   kmVisible.innerText = kmprice;
 });
