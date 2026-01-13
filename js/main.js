16f50750f103d1669cdef040eacddc9ee2d34e41
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-01-13 15:47:30 +0100

    order

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
+// added variables HTML
 const formVerifyer = document.getElementById("verifyer");
 const kmCounter = document.getElementById("km-counter");
 const kmVisible = document.getElementById("km-price");
 const announcer = document.getElementById("announcer");
 const ageFinder = document.getElementById("age-finder");
-console.log(formVerifyer);
+// FORM FUNCTION
 formVerifyer.addEventListener("submit", function (e) {
   e.preventDefault();
+  //   price calculator
   let userKm = kmCounter.value;
   let kmPrice = userKm * 0.21;
   console.log(kmPrice);
+  //   if sales
   if (ageFinder.value === "over-65") {
     kmPrice = kmPrice - (kmPrice * 40) / 100;
     let kmPriceDecimal = kmPrice.toFixed(2);
@@ -17,6 +20,7 @@ formVerifyer.addEventListener("submit", function (e) {
     kmPrice = kmPrice - (kmPrice * 20) / 100;
     let kmPriceDecimal = kmPrice.toFixed(2);
     kmVisible.innerText = kmPriceDecimal;
+    // normal price
   } else {
     announcer.innerText = "Il prezzo è ";
     kmVisible.innerText = kmPrice;
